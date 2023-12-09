d44c78ad86b92cd856872116ff7bccd57a2250f8
Diana Dykun <diana.dykoon@gmail.com>
2023-12-09 21:16:26 +0200

    Реалізовано запит Query1

--- Program.cs
+++ Program.cs
@@ -48,15 +48,17 @@ namespace Practice_Linq
         {
             //Query 1: Вивести всі матчі, які відбулися в Україні у 2012 році.
 
-            var selectedGames = games; // Корегуємо запит !!!
+            var selectedGames = games.Where(p => p.Country == "Ukraine" && p.Date.Year == 2012).ToList(); // Корегуємо запит !!!
 
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 1 ========================");
 
             // див. приклад як має бути виведено:
-
-
+            foreach (var game in selectedGames)
+            {
+                Console.WriteLine($"{game.Date:d.MM.yyyy} {game.Home_team} - {game.Away_team}, Score: {game.Home_score} - {game.Away_score}, Country: {game.Country}");
+            }
         }
 
         // Запит 2
